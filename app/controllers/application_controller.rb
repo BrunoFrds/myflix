448a3164903a46ab7cddee3944970687070dac43
Bruno Fernandes <brunofrdsdev@gmail.com>
2025-05-19 16:54:44 +0200

    ajout des champs first_name et last_name au modele user + update des controllers application et pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+  # Exige que l'utilisateur soit connecté pour toutes les actions
+  before_action :authenticate_user!
+  # Autorise des paramètres personnalisés pour Devise
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  # Autorise les champs supplémentaires pour l'inscription et la mise à jour du compte utilisateur
+  def configure_permitted_parameters
+    # Permet d'ajouter les champs first_name et last_name lors de l'inscription
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name])
+
+    ## Permet d'ajouter les champs first_name et last_name lors de la mise à jour du compte
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name])
+  end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
+  # Autorise l'accès à la page d'accueil sans authentification
+  skip_before_action :authenticate_user!, only: :home
+
   def home
   end
 end
